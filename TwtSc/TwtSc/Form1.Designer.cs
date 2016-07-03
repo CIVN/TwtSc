f7404b30484c568552a024440f7ebb1933c3ae4d
CIVN <civn.cf.storage@gmail.com>
2016-07-03 10:56:07 +0900

    First Upload

---
+++ TwtSc/TwtSc/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace TwtSc
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.認証ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.リセットToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ヘルプToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.twitteroktmstyleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(38, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 12);
+			this.label1.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.認証ToolStripMenuItem,
+            this.リセットToolStripMenuItem,
+            this.ヘルプToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+			this.menuStrip1.TabIndex = 3;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// 認証ToolStripMenuItem
+			// 
+			this.認証ToolStripMenuItem.Name = "認証ToolStripMenuItem";
+			this.認証ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+			this.認証ToolStripMenuItem.Text = "認証";
+			this.認証ToolStripMenuItem.Click += new System.EventHandler(this.認証ToolStripMenuItem_Click);
+			// 
+			// リセットToolStripMenuItem
+			// 
+			this.リセットToolStripMenuItem.Name = "リセットToolStripMenuItem";
+			this.リセットToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+			this.リセットToolStripMenuItem.Text = "リセット";
+			this.リセットToolStripMenuItem.Click += new System.EventHandler(this.リセットToolStripMenuItem_Click);
+			// 
+			// ヘルプToolStripMenuItem
+			// 
+			this.ヘルプToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.twitteroktmstyleToolStripMenuItem});
+			this.ヘルプToolStripMenuItem.Name = "ヘルプToolStripMenuItem";
+			this.ヘルプToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.ヘルプToolStripMenuItem.Text = "ヘルプ";
+			// 
+			// twitteroktmstyleToolStripMenuItem
+			// 
+			this.twitteroktmstyleToolStripMenuItem.Name = "twitteroktmstyleToolStripMenuItem";
+			this.twitteroktmstyleToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+			this.twitteroktmstyleToolStripMenuItem.Text = "Twitter(@oktm_style)";
+			this.twitteroktmstyleToolStripMenuItem.Click += new System.EventHandler(this.twitteroktmstyleToolStripMenuItem_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(10, 83);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(25, 12);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "PIN:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(40, 80);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(70, 19);
+			this.textBox1.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(116, 78);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(30, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "OK";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 111);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.menuStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.KeyPreview = true;
+			this.MainMenuStrip = this.menuStrip1;
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(300, 150);
+			this.MinimumSize = new System.Drawing.Size(300, 150);
+			this.Name = "Form1";
+			this.Text = "TwtSc";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem 認証ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem リセットToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ヘルプToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem twitteroktmstyleToolStripMenuItem;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ TwtSc/TwtSc/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using CoreTweet;
+using static CoreTweet.OAuth;
+using static TwtSc.Properties.Settings;
+
+namespace TwtSc
+{
+	public partial class Form1 : Form
+	{
+		private HotKey hotKey;
+
+		private Tokens token;
+		private OAuthSession session;
+
+		private string PATH = Environment.CurrentDirectory + "\\" + "ScreenShots";
+
+		private const string CONSUMER_KEY = "ARvucpPIcosQBdehQKKH93HhT";
+		private const string CONSUMER_SECRET = "5LJh1LNuUrx6C8bXdxBceFYYQZdM5YeHodR9mym3Q1zfix5IG3";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			session = Authorize(CONSUMER_KEY, CONSUMER_SECRET);
+
+			if (!(Default.AccessToken == "" && Default.AccessTokenSecret == "" && Default.userID == 0 && Default.screenName == ""))
+			{
+				token = Tokens.Create(CONSUMER_KEY, CONSUMER_SECRET, Default.AccessToken, Default.AccessTokenSecret, Default.userID, Default.screenName);
+
+				if (token != null)
+				{
+					ShowInfomations(token);
+					HideControls();
+				}
+			}
+
+			hotKey = new HotKey(MOD_KEY.SHIFT, Keys.PrintScreen);
+			hotKey.HotKeyPush += new EventHandler(hotKey_HotKeyPush);
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			hotKey.Dispose();
+		}
+
+		private void hotKey_HotKeyPush(object sender, EventArgs e)
+		{
+			if (!(Directory.Exists(PATH)))
+			{
+				var info = Directory.CreateDirectory(PATH);
+
+				if (info.Exists == false)
+				{
+					MessageBox.Show("ファイルを保存するフォルダが存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return;
+				}
+			}
+
+			var bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+			var g = Graphics.FromImage(bmp);
+			g.CopyFromScreen(new Point(0, 0), new Point(0, 0), bmp.Size);
+			g.Dispose();
+
+			var file = PATH + "\\" + DateTime.Now.ToFileTime() + ".png";
+
+			bmp.Save(file, ImageFormat.Png);
+
+			var player = new System.Media.SoundPlayer(Properties.Resources.シャッター音);
+			player.Play();
+
+			token.Statuses.UpdateWithMediaAsync(status => "", media => new FileInfo(file));
+		}
+
+		private void HideControls()
+		{
+			menuStrip1.Items.Remove(認証ToolStripMenuItem);
+			label2.Hide();
+			textBox1.Hide();
+			button1.Hide();
+		}
+
+		private void ShowInfomations(Tokens token)
+		{
+			label1.Text = "@" + token.ScreenName;
+			pictureBox1.ImageLocation = token.Users.Show(token.ScreenName).ProfileImageUrl;
+		}
+
+		private void 認証ToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Process.Start(session.AuthorizeUri.OriginalString);
+		}
+
+		private void リセットToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Default.Reset();
+			Default.Save();
+
+			token = null;
+
+			Application.Restart();
+		}
+
+		private void twitteroktmstyleToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Process.Start("https://twitter.com/oktm_style/");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var pin = textBox1.Text;
+
+			try
+			{
+				token = session.GetTokens(pin);
+			}
+
+			catch
+			{
+				MessageBox.Show("認証に失敗しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			if (token != null)
+			{
+				MessageBox.Show("認証に成功しました。", "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+				Default.AccessToken = token.AccessToken;
+				Default.AccessTokenSecret = token.AccessTokenSecret;
+				Default.userID = token.UserId;
+				Default.screenName = token.ScreenName;
+				Default.Save();
+
+				ShowInfomations(token);
+				HideControls();
+			}
+		}
+	}
+}
